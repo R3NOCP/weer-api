1577e67cbd1faef2c6e0e4e5cbaa5d6fa942b78f
Reno Postema <postemareno@gmail.com>
2019-05-10 15:36:23 +0200

    show temp

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const app = document.getElementById('liveweer');
-    const logo = document.createElement('img');
-    logo.src = 'https://www.iphone.nl/uploads/cgblog/id370/weeronline.nl_downloaden.jpg';
+const app = document.getElementById('liveweer')
 
-    const container = document.createElement('div');
-    container.setAttribute('class', 'container');
+const logo = document.createElement('img')
+logo.src = 'https://www.iphone.nl/uploads/cgblog/id370/weeronline.nl_downloaden.jpg'
 
-    app.appendChild(logo);
-    app.appendChild(container);
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://weerlive.nl/api/json-data-10min.php?key=f97777689c&locatie=Amsterdam', true);
-    xhr.onload = function() {
-        // Begin accessing JSON data here
-        console.log(xhr.responseText);
-        var data = JSON.parse(xhr.responseText);
+app.appendChild(logo)
+app.appendChild(container)
 
-        if (xhr.status >= 200 && xhr.status < 400) {
-            console.log(data);
+var xhr = new XMLHttpRequest()
+xhr.open('GET', 'https://weerlive.nl/api/json-data-10min.php?key=f97777689c&locatie=Amsterdam', true)
+xhr.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(xhr.responseText)
+  if (xhr.status >= 200 && xhr.status < 400) {
+    data.liveweer.forEach(liveweer => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
 
+      const h1 = document.createElement('h1')
+      h1.textContent = liveweer.temp
 
-            data.liveweer.forEach(function(element) {
-                console.log(element.plaats);
-                console.log(element.temp);
-            });
-        } else {
-            console.log('error')
-        }
-    };
+      const h2 = document.createElement('h2')
+      h2.textContent = liveweer.plaats
 
-    xhr.send();
+      container.appendChild(card)
+      card.appendChild(h1)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Oops!`
+    app.appendChild(errorMessage)
+  }
+}
+
+xhr.send()
